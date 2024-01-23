3244550ea0d7fdc5ad23119f5a31ccdd325b7fb0
Andre Microsoft <andreidal@andres-mbp-2.lan>
2024-01-23 13:58:37 -0600

    Resolves #2

--- Application/tests/RazorPagesTestSample.Tests/UnitTests/DataAccessLayerTest.cs
+++ Application/tests/RazorPagesTestSample.Tests/UnitTests/DataAccessLayerTest.cs
@@ -127,4 +127,29 @@ namespace RazorPagesTestSample.Tests.UnitTests
         }
         #endregion
     }
+    
+  [Theory]
+  [InlineData(150, true)]
+  [InlineData(199, true)]
+  [InlineData(200, true)]
+  [InlineData(201, true)]
+  [InlineData(249, true)]
+  [InlineData(250, true)]
+  [InlineData(251, false)]
+  [InlineData(300, false)]
+  public async Task AddMessageAsync_TestMessageLength(int messageLength, bool expectedValidMessage)
+  {
+      using (var db = new AppDbContext(Utilities.TestDbContextOptions()))
+      {
+          // Arrange
+          var recId = 10;
+          var expectedMessage = new Message() { Id = recId, Text = new string('X', messageLength) };
+
+          // Act
+          var isValidMessage = Validator.TryValidateObject(expectedMessage, new ValidationContext(expectedMessage), null, validateAllProperties: true);
+
+          // Assert
+          Assert.Equal(expectedValidMessage, isValidMessage);
+      }
+  }
 }
