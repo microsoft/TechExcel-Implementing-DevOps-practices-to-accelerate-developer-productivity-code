69671178452e12f4bdea5ebe1fb34bfdda86c1d5
Lino Tadros <thetrainingboss@gmail.com>
2024-02-27 21:20:40 -0500

    devops additions 0303

--- Application/src/RazorPagesTestSample/Data/Message.cs
+++ Application/src/RazorPagesTestSample/Data/Message.cs
@@ -9,10 +9,11 @@ namespace RazorPagesTestSample.Data
 
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
+        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion
 
     // Write a loop from 0 to 10 and print out the numeric value of the iterator for each loop iteration
+    
 }
